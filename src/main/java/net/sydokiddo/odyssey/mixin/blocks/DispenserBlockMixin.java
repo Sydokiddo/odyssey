698c31981da1586e78174de6350e5eb1a654dc7f
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2023-07-24 07:02:32 -0700

    Frog, Squid, and Glow Squid Buckets can now be dispensed from Dispensers

---
+++ src/main/java/net/sydokiddo/odyssey/mixin/blocks/DispenserBlockMixin.java
@@ -0,0 +1,28 @@
+package net.sydokiddo.odyssey.mixin.blocks;
+
+import net.minecraft.core.dispenser.DispenseItemBehavior;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.DispenserBlock;
+import net.sydokiddo.chrysalis.misc.util.dispenser.DispenseBucketMobDispenserBehavior;
+import net.sydokiddo.odyssey.registry.items.ModItems;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(DispenserBlock.class)
+public class DispenserBlockMixin {
+
+    // Custom Dispenser Methods
+
+    @Inject(method = "getDispenseMethod", at = @At("HEAD"), cancellable = true)
+    private void odyssey_addNewDispenserMethod(ItemStack itemStack, CallbackInfoReturnable<DispenseItemBehavior> cir) {
+
+        Item item = itemStack.getItem();
+
+        if (item == ModItems.FROG_BUCKET || item == ModItems.SQUID_BUCKET || item == ModItems.GLOW_SQUID_BUCKET) {
+            cir.setReturnValue(DispenseBucketMobDispenserBehavior.INSTANCE);
+        }
+    }
+}
