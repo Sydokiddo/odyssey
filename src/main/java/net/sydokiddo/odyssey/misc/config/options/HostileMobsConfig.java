8b22003c76e41d098e67886accf8a93855c70bae
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2023-11-16 22:16:19 -0800

    Charged Creeper Head Drop Tweak

--- src/main/java/net/sydokiddo/odyssey/misc/config/options/HostileMobsConfig.java
+++ src/main/java/net/sydokiddo/odyssey/misc/config/options/HostileMobsConfig.java
@@ -12,4 +12,7 @@ public class HostileMobsConfig {
 
     @ConfigEntry.Gui.Tooltip
     public boolean slime_and_magma_cube_converting = true;
+
+    @ConfigEntry.Gui.Tooltip
+    public boolean more_heads_from_charged_creepers = true;
 }
