76ba85663581681ac26baebdd43df838b556abe2
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2023-11-16 22:45:51 -0800

    Improved Shield Tooltip

---
+++ src/main/java/net/sydokiddo/odyssey/mixin/items/tools/CrossbowItemMixin.java
@@ -0,0 +1,28 @@
+package net.sydokiddo.odyssey.mixin.items.tools;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CrossbowItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ProjectileWeaponItem;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import net.sydokiddo.odyssey.registry.OdysseyRegistry;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import java.util.List;
+
+@Mixin(CrossbowItem.class)
+public abstract class CrossbowItemMixin extends ProjectileWeaponItem {
+
+    private CrossbowItemMixin(Properties properties) {
+        super(properties);
+    }
+
+    @Inject(method = "appendHoverText", at = @At("HEAD"))
+    private void odyssey$changeCrossbowTooltip(ItemStack itemStack, @Nullable Level level, List<Component> tooltip, TooltipFlag tooltipFlag, CallbackInfo ci) {
+        OdysseyRegistry.addItemDurabilityTooltip(itemStack, tooltip, tooltipFlag);
+    }
+}
--- src/main/java/net/sydokiddo/odyssey/mixin/items/tools/ShieldItemMixin.java
+++ src/main/java/net/sydokiddo/odyssey/mixin/items/tools/ShieldItemMixin.java
@@ -1,16 +1,19 @@
 package net.sydokiddo.odyssey.mixin.items.tools;
 
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.Holder;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.ShieldItem;
-import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BannerPattern;
 import net.sydokiddo.chrysalis.registry.ChrysalisRegistry;
 import net.sydokiddo.odyssey.Odyssey;
 import net.sydokiddo.odyssey.registry.OdysseyRegistry;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,12 +22,50 @@ import java.util.List;
 @Mixin(ShieldItem.class)
 public class ShieldItemMixin {
 
-    @Inject(method = "appendHoverText", at = @At("RETURN"))
+    @Unique
+    private static void addBannerTextToShield(ItemStack itemStack, List<Component> list) {
+
+        CompoundTag blockEntityData = BlockItem.getBlockEntityData(itemStack);
+        String patternsString = "Patterns";
+
+        if (blockEntityData == null || !blockEntityData.contains(patternsString)) {
+            return;
+        }
+
+        ListTag listTag = blockEntityData.getList(patternsString, 10);
+
+        for (int i = 0; i < listTag.size() && i < 6; ++i) {
+
+            CompoundTag compoundTag = listTag.getCompound(i);
+
+            DyeColor dyeColor = DyeColor.byId(compoundTag.getInt("Color"));
+            Holder<BannerPattern> holder = BannerPattern.byHash(compoundTag.getString("Pattern"));
+
+            if (holder == null) continue;
+
+            holder.unwrapKey().map(resourceKey -> resourceKey.location().toShortLanguageKey()).ifPresent(string -> list.add(CommonComponents.space().append(Component.translatable("block.minecraft.banner." + string + "." + dyeColor.getName()).withStyle(ChatFormatting.BLUE))));
+        }
+    }
+
+    @Inject(method = "appendHoverText", at = @At("HEAD"), cancellable = true)
     private void odyssey$addShieldTooltip(ItemStack itemStack, Level level, List<Component> tooltip, TooltipFlag tooltipFlag, CallbackInfo ci) {
+
+        CompoundTag compoundTag = BlockItem.getBlockEntityData(itemStack);
+
         if (Odyssey.getConfig().items.tooltipConfig.shields) {
+
+            ci.cancel();
             OdysseyRegistry.addItemDurabilityTooltip(itemStack, tooltip, tooltipFlag);
+
+            if (compoundTag != null && compoundTag.contains("Patterns")) {
+                tooltip.add(CommonComponents.EMPTY);
+                tooltip.add(Component.translatable("gui.odyssey.item.shield.banner").withStyle(ChatFormatting.GRAY));
+                addBannerTextToShield(itemStack, tooltip);
+            }
+
             ChrysalisRegistry.addUseTooltip(tooltip);
             tooltip.add(CommonComponents.space().append(Component.translatable("item.odyssey.shield.desc").withStyle(ChatFormatting.BLUE)));
+
             OdysseyRegistry.addSpaceOnTooltipIfEnchantedOrTrimmed(itemStack, tooltip);
         }
     }
