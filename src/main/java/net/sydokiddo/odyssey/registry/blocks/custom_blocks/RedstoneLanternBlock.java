e456af483bcd291b6648afc848351f0291747cbb
Sydney <94016034+Sydokiddo@users.noreply.github.com>
2024-01-21 21:14:01 -0800

    Redstone Lanterns now react to Breeze Wind Charges

--- src/main/java/net/sydokiddo/odyssey/registry/blocks/custom_blocks/RedstoneLanternBlock.java
+++ src/main/java/net/sydokiddo/odyssey/registry/blocks/custom_blocks/RedstoneLanternBlock.java
@@ -11,7 +11,9 @@ import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.LanternBlock;
@@ -23,7 +25,9 @@ import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.BlockHitResult;
 import net.sydokiddo.odyssey.registry.misc.ModSoundEvents;
 import org.jetbrains.annotations.NotNull;
+import java.util.function.BiConsumer;
 
+@SuppressWarnings("deprecation")
 public class RedstoneLanternBlock extends LanternBlock {
 
     private final int tickTime = 2;
@@ -41,31 +45,23 @@ public class RedstoneLanternBlock extends LanternBlock {
         builder.add(HANGING, LIT, WATERLOGGED);
     }
 
-    @SuppressWarnings("deprecation")
     @Override
-    public void onPlace(BlockState blockState, Level level, BlockPos blockPos, BlockState adjacentBlockState, boolean bl) {
-        for (Direction direction : Direction.values()) {
-            level.updateNeighborsAt(blockPos.relative(direction), this);
-        }
+    public void onPlace(BlockState blockState, Level level, BlockPos blockPos, BlockState adjacentBlockState, boolean moved) {
+        this.updateRedstoneLanternNeighbors(level, blockPos);
     }
 
-    @SuppressWarnings("deprecation")
     @Override
     public void onRemove(BlockState blockState, Level level, BlockPos blockPos, BlockState adjacentBlockState, boolean moved) {
         if (!moved) {
-            for (Direction direction : Direction.values()) {
-                level.updateNeighborsAt(blockPos.relative(direction), this);
-            }
+            this.updateRedstoneLanternNeighbors(level, blockPos);
         }
     }
 
-    @SuppressWarnings("deprecation")
     @Override
     public boolean isSignalSource(BlockState blockState) {
         return true;
     }
 
-    @SuppressWarnings("deprecation")
     @Override
     public int getSignal(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, Direction direction) {
         return blockState.getValue(LIT) ? 15 : 0;
@@ -75,29 +71,33 @@ public class RedstoneLanternBlock extends LanternBlock {
 
     // region Interacting
 
-    @SuppressWarnings("deprecation")
     @Override
     public @NotNull InteractionResult use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult) {
 
         if (blockState.getValue(LIT)) {
-            turnOffLantern(level, blockPos, blockState);
+            this.turnOffLantern(level, blockPos, blockState);
         } else {
-            turnOnLantern(level, blockPos, blockState);
-        }
-
-        level.updateNeighborsAt(blockPos, this);
-
-        for (Direction direction : Direction.values()) {
-            level.updateNeighborsAt(blockPos.relative(direction), this);
+            this.turnOnLantern(level, blockPos, blockState);
         }
 
         return InteractionResult.sidedSuccess(level.isClientSide());
+    }
+
+    @Override
+    public void onExplosionHit(BlockState blockState, Level level, BlockPos blockPos, Explosion explosion, BiConsumer<ItemStack, BlockPos> biConsumer) {
+
+        if (!level.isClientSide() && explosion.getBlockInteraction() == Explosion.BlockInteraction.TRIGGER_BLOCK && blockState.getValue(LIT)) {
+            this.turnOffLantern(level, blockPos, blockState);
+        }
+
+        super.onExplosionHit(blockState, level, blockPos, explosion, biConsumer);
     }
 
     private void turnOnLantern(Level level, BlockPos blockPos, BlockState blockState) {
         level.playSound(null, blockPos, ModSoundEvents.REDSTONE_LANTERN_POWER_ON, SoundSource.BLOCKS);
         level.setBlock(blockPos, blockState.setValue(LIT, true), tickTime);
         level.gameEvent(null, GameEvent.BLOCK_ACTIVATE, blockPos);
+        this.updateRedstoneLanternNeighbors(level, blockPos);
     }
 
     private void turnOffLantern(Level level, BlockPos blockPos, BlockState blockState) {
@@ -105,10 +105,11 @@ public class RedstoneLanternBlock extends LanternBlock {
         level.playSound(null, blockPos, ModSoundEvents.REDSTONE_LANTERN_POWER_OFF, SoundSource.BLOCKS);
         level.setBlock(blockPos, blockState.setValue(LIT, false), tickTime);
         level.gameEvent(null, GameEvent.BLOCK_DEACTIVATE, blockPos);
+        this.updateRedstoneLanternNeighbors(level, blockPos);
 
         if (level instanceof ServerLevel serverLevel) {
 
-            for (int i = 0; i < 10; ++i) {
+            for (int particleAmount = 0; particleAmount < 10; ++particleAmount) {
 
                 SimpleParticleType particleType;
 
@@ -123,21 +124,31 @@ public class RedstoneLanternBlock extends LanternBlock {
         }
     }
 
+    private void updateRedstoneLanternNeighbors(Level level, BlockPos blockPos) {
+
+        level.updateNeighborsAt(blockPos, this);
+
+        for (Direction direction : Direction.values()) {
+            level.updateNeighborsAt(blockPos.relative(direction), this);
+        }
+    }
+
     // endregion
 
     // region Particles
 
     @Override
     public void animateTick(BlockState blockState, Level level, BlockPos blockPos, RandomSource randomSource) {
+
         if (blockState.getValue(LIT)) {
 
             Direction direction = Direction.getRandom(randomSource);
 
             if (direction != Direction.UP) {
-                double d = direction.getStepX() == 0 ? randomSource.nextDouble() : 0.3D + (double) direction.getStepX() * 0.1D;
-                double e = direction.getStepY() == 0 ? randomSource.nextDouble() : 0.3D + (double) direction.getStepY() * 0.1D;
-                double f = direction.getStepZ() == 0 ? randomSource.nextDouble() : 0.3D + (double) direction.getStepZ() * 0.1D;
-                level.addParticle(DustParticleOptions.REDSTONE, (double) blockPos.getX() + d, (double) blockPos.getY() + e, (double) blockPos.getZ() + f, 0.0D, 0.0D, 0.0D);
+                double x = direction.getStepX() == 0 ? randomSource.nextDouble() : 0.3D + (double) direction.getStepX() * 0.1D;
+                double y = direction.getStepY() == 0 ? randomSource.nextDouble() : 0.3D + (double) direction.getStepY() * 0.1D;
+                double z = direction.getStepZ() == 0 ? randomSource.nextDouble() : 0.3D + (double) direction.getStepZ() * 0.1D;
+                level.addParticle(DustParticleOptions.REDSTONE, (double) blockPos.getX() + x, (double) blockPos.getY() + y, (double) blockPos.getZ() + z, 0.0D, 0.0D, 0.0D);
             }
         }
     }
